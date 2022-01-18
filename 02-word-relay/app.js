6b946c4bae69150be940f96544c2e68730fc0bc4
theseda2s <theseda2s@naver.com>
2022-01-18 18:22:57 +0900

    word relay game

---
+++ 02-word-relay/app.js
@@ -0,0 +1,68 @@
+const number = parseInt(prompt('몇 명이 참가하나요?'), 10);
+const $button = document.querySelector('button');
+const $input = document.querySelector('input');
+const $word = document.querySelector('#word');
+const $order = document.querySelector('#order');
+let word;
+let newWord;
+/* 
+const onClickButton = () => {
+  // 제시어가 비어있는가?
+  if (!word) {
+    word = newWord; // 입력한 단어가 제시어가 된다.
+    $word.textContent = word; // 화면 바꿔줌
+    const order = parseInt($order.textContent); // 현재 순서
+    if (order + 1 > number) {
+      $order.textContent = 1;
+    } else {
+      $order.textContent = order + 1;
+    }
+    $input.value = '';
+    $input.focus();
+  } else {
+    if (word[word.length - 1] == newWord[0]) {
+      // 올바른가
+      word = newWord;
+      $word.textContent = word;
+      const order = parseInt($order.textContent); // 현재 순서
+      if (order + 1 > number) {
+        $order.textContent = 1;
+      } else {
+        $order.textContent = order + 1;
+      }
+      $input.value = '';
+      $input.focus();
+    } else {
+      // 올바르지 않은가
+      alert('올바르지 않은 단어입니다.');
+      $input.value = '';
+      $input.focus();
+    }
+  }
+};
+*/
+const onClickButton = () => {
+  // 제시어가 비어있거나 입력한 단어가 올바른가?
+  if (!word || word[word.length - 1] == newWord[0]) {
+    word = newWord; // 입력한 단어가 제시어가 된다.
+    $word.textContent = word; // 화면 바꿔줌
+    const order = parseInt($order.textContent); // 현재 순서
+    if (order + 1 > number) {
+      $order.textContent = 1;
+    } else {
+      $order.textContent = order + 1;
+    }
+  } else {
+    alert('올바르지 않은 단어입니다.');
+  }
+  $input.value = '';
+  $input.focus();
+};
+
+// 새 단어 제시어로 저장
+const onInput = (event) => {
+  newWord = event.target.value;
+};
+
+$button.addEventListener('click', onClickButton);
+$input.addEventListener('input', onInput);
---
+++ 02-word-relay/self-check/app.js
@@ -0,0 +1,39 @@
+const number = parseInt(prompt('몇 명이 참가하나요?'), 10);
+
+if (number) {
+  const $button = document.querySelector('button');
+  const $input = document.querySelector('input');
+  const $word = document.querySelector('#word');
+  const $order = document.querySelector('#order');
+  let word;
+  let newWord;
+  const onClickButton = () => {
+    // 세글자이면서 제시어가 비어있거나 입력한 단어가 올바른가?
+    if (newWord.length === 3 && (!word || word[word.length - 1] === newWord[0])) {
+      word = newWord; // 입력한 단어가 제시어가 된다.
+      $word.textContent = word; // 화면 바꿔줌
+      const order = parseInt($order.textContent); // 현재 순서
+      if (order + 1 > number) {
+        $order.textContent = 1;
+      } else {
+        $order.textContent = order + 1;
+      }
+    } else if (newWord.length != 3) {
+      alert('단어는 3글자여야 합니다.');
+    } else {
+      alert('올바르지 않은 단어입니다.');
+    }
+    $input.value = '';
+    $input.focus();
+  };
+
+  // 새 단어 제시어로 저장
+  const onInput = (event) => {
+    newWord = event.target.value;
+  };
+
+  $button.addEventListener('click', onClickButton);
+  $input.addEventListener('input', onInput);
+} else {
+  alert('게임이 취소되었습니다.');
+}
