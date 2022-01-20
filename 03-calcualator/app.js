a8960ec9a2101b7b1f8ab1b540ae60bd411c5dcd
theseda2s <theseda2s@naver.com>
2022-01-20 18:56:18 +0900

    calculator

---
+++ 03-calcualator/app.js
@@ -0,0 +1,73 @@
+let numOne = '';
+let operator = '';
+let numTwo = '';
+const $operator = document.querySelector('#operator');
+const $result = document.querySelector('#result');
+const onClickNumber = (event) => {
+  if (!operator) {
+    numOne += event.target.textContent;
+    $result.value += event.target.textContent;
+    return;
+  }
+  if (!numTwo) {
+    $result.value = '';
+  }
+  numTwo += event.target.textContent;
+  $result.value += event.target.textContent;
+};
+const onClickOperator = (op) => () => {
+  if (numOne) {
+    operator += op;
+    $operator.value = op;
+  } else {
+    alert('숫자를 먼저 입력하세요.');
+  }
+};
+
+document.querySelector('#num-0').addEventListener('click', onClickNumber);
+document.querySelector('#num-1').addEventListener('click', onClickNumber);
+document.querySelector('#num-2').addEventListener('click', onClickNumber);
+document.querySelector('#num-3').addEventListener('click', onClickNumber);
+document.querySelector('#num-4').addEventListener('click', onClickNumber);
+document.querySelector('#num-5').addEventListener('click', onClickNumber);
+document.querySelector('#num-6').addEventListener('click', onClickNumber);
+document.querySelector('#num-7').addEventListener('click', onClickNumber);
+document.querySelector('#num-8').addEventListener('click', onClickNumber);
+document.querySelector('#num-9').addEventListener('click', onClickNumber);
+document.querySelector('#plus').addEventListener('click', onClickOperator('+'));
+document.querySelector('#minus').addEventListener('click', onClickOperator('-'));
+document.querySelector('#divide').addEventListener('click', onClickOperator('/'));
+document.querySelector('#clear').addEventListener('click', () => {
+  numOne = '';
+  operator = '';
+  numTwo = '';
+  $result.value = '';
+  $operator.value = '';
+});
+document.querySelector('#calculate').addEventListener('click', () => {
+  if (numTwo) {
+    switch (operator) {
+      case '+':
+        $result.value = parseInt(numOne) + parseInt(numTwo);
+        break;
+      case '-':
+        $result.value = numOne - numTwo;
+        break;
+      case '*':
+        $result.value = numOne * numTwo;
+        break;
+      case '/':
+        $result.value = numOne / numTwo;
+        break;
+      default:
+        break;
+    }
+    $operator.value = '';
+    numOne = $result.value;
+    operator = '';
+    numTwo = '';
+  } else {
+    alert('숫자를 먼저 입력하세요.');
+  }
+});
+document.querySelector('#multiply').addEventListener('click', onClickOperator('*'));
